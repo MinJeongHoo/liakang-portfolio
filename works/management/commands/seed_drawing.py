0868e6b8a8be0c1192c5d8c21aa0cfe0a082f232
MinJeongHoo <msw1302@gmail.com>
2020-12-11 14:53:23 +0900

    aa

--- works/management/commands/seed_drawing.py
+++ works/management/commands/seed_drawing.py
@@ -5,10 +5,6 @@ from works.models import WorkSize
 from django.contrib.admin.utils import flatten
 from workcategory.models import WorksCategoryModel
 import cloudinary
-import os
-from PIL import Image
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-MEDIA_ROOT = os.path.join(BASE_DIR, "uploads")
 
 
 class Command(BaseCommand):
@@ -21,144 +17,76 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         arr = [
-            ["7", "Drawing", "", "", "", Image.open(
-                MEDIA_ROOT+"/drawing/1.jpg")],
-            ["7", "Drawing", "", "", "", Image.open(
-                MEDIA_ROOT+"/drawing/2.jpg")],
-            ["7", "Drawing", "", "", "", Image.open(
-                MEDIA_ROOT+"/drawing/3.jpg")],
-            ["7", "Drawing", "", "", "", Image.open(
-                MEDIA_ROOT+"/drawing/4.jpg")],
-            ["7", "Drawing", "", "", "", Image.open(
-                MEDIA_ROOT+"/drawing/5.jpg")],
-            ["7", "Drawing", "", "", "", Image.open(
-                MEDIA_ROOT+"/drawing/6.jpg")],
-            ["7", "Drawing", "", "", "", Image.open(
-                MEDIA_ROOT+"/drawing/7.jpg")],
-            ["7", "Drawing", "", "", "", Image.open(
-                MEDIA_ROOT+"/drawing/8.jpg")],
-            ["7", "Drawing", "", "", "", Image.open(
-                MEDIA_ROOT+"/drawing/9.jpg")],
-            ["7", "Drawing", "", "", "", Image.open(
-                MEDIA_ROOT+"/drawing/10.jpg")],
-            ["7", "Drawing", "", "", "", Image.open(
-                MEDIA_ROOT+"/drawing/11.jpg")],
-            ["7", "Drawing", "", "", "", Image.open(
-                MEDIA_ROOT+"/drawing/12.jpg")],
-            ["7", "Drawing", "", "", "", Image.open(
-                MEDIA_ROOT+"/drawing/13.jpg")],
-            ["7", "Drawing", "", "", "", Image.open(
-                MEDIA_ROOT+"/drawing/14.jpg")],
-            ["7", "Drawing", "", "", "", Image.open(
-                MEDIA_ROOT+"/drawing/15.jpg")],
-            ["7", "Drawing", "", "", "", Image.open(
-                MEDIA_ROOT+"/drawing/16.jpg")],
-            ["7", "Drawing", "", "", "", Image.open(
-                MEDIA_ROOT+"/drawing/17.jpg")],
-            ["7", "Drawing", "", "", "", Image.open(
-                MEDIA_ROOT+"/drawing/18.jpg")],
-            ["7", "Drawing", "", "", "", Image.open(
-                MEDIA_ROOT+"/drawing/19.jpg")],
-            ["7", "Drawing", "", "", "", Image.open(
-                MEDIA_ROOT+"/drawing/20.jpg")],
-            ["7", "Drawing", "", "", "", Image.open(
-                MEDIA_ROOT+"/drawing/21.jpg")],
-            ["7", "Drawing", "", "", "", Image.open(
-                MEDIA_ROOT+"/drawing/22.jpg")],
-            ["7", "Drawing", "", "", "", Image.open(
-                MEDIA_ROOT+"/drawing/23.jpg")],
-            ["7", "Drawing", "", "", "", Image.open(
-                MEDIA_ROOT+"/drawing/24.jpg")],
-            ["7", "Drawing", "", "", "", Image.open(
-                MEDIA_ROOT+"/drawing/25.jpg")],
-            ["7", "Drawing", "", "", "", Image.open(
-                MEDIA_ROOT+"/drawing/26.jpg")],
-            ["7", "Drawing", "", "", "", Image.open(
-                MEDIA_ROOT+"/drawing/27.jpg")],
-            ["7", "Drawing", "", "", "", Image.open(
-                MEDIA_ROOT+"/drawing/28.jpg")],
-            ["7", "Drawing", "", "", "", Image.open(
-                MEDIA_ROOT+"/drawing/29.jpg")],
-            ["7", "Drawing", "", "", "", Image.open(
-                MEDIA_ROOT+"/drawing/30.jpg")],
-            ["7", "Drawing", "", "", "", Image.open(
-                MEDIA_ROOT+"/drawing/31.jpg")],
-            ["7", "Drawing", "", "", "", Image.open(
-                MEDIA_ROOT+"/drawing/32.jpg")],
-            ["7", "Drawing", "", "", "", Image.open(
-                MEDIA_ROOT+"/drawing/33.jpg")],
-            ["7", "Drawing", "", "", "", Image.open(
-                MEDIA_ROOT+"/drawing/34.jpg")],
-            ["7", "Drawing", "", "", "", Image.open(
-                MEDIA_ROOT+"/drawing/35.jpg")],
-            ["7", "Drawing", "", "", "", Image.open(
-                MEDIA_ROOT+"/drawing/36.jpg")],
-            ["7", "Drawing", "", "", "", Image.open(
-                MEDIA_ROOT+"/drawing/37.jpg")],
-            ["7", "Drawing", "", "", "", Image.open(
-                MEDIA_ROOT+"/drawing/38.jpg")],
-            ["7", "Drawing", "", "", "", Image.open(
-                MEDIA_ROOT+"/drawing/39.jpg")],
-            ["7", "Drawing", "", "", "", Image.open(
-                MEDIA_ROOT+"/drawing/40.jpg")],
-            ["7", "Drawing", "", "", "", Image.open(
-                MEDIA_ROOT+"/drawing/41.jpg")],
-            ["7", "Drawing", "", "", "", Image.open(
-                MEDIA_ROOT+"/drawing/42.jpg")],
-            ["7", "Drawing", "", "", "", Image.open(
-                MEDIA_ROOT+"/drawing/43.jpg")],
-            ["7", "Drawing", "", "", "", Image.open(
-                MEDIA_ROOT+"/drawing/44.jpg")],
-            ["7", "Drawing", "", "", "", Image.open(
-                MEDIA_ROOT+"/drawing/45.jpg")],
-            ["7", "Drawing", "", "", "", Image.open(
-                MEDIA_ROOT+"/drawing/46.jpg")],
-            ["7", "Drawing", "", "", "", Image.open(
-                MEDIA_ROOT+"/drawing/47.jpg")],
-            ["7", "Drawing", "", "", "", Image.open(
-                MEDIA_ROOT+"/drawing/48.jpg")],
-            ["7", "Drawing", "", "", "", Image.open(
-                MEDIA_ROOT+"/drawing/49.jpg")],
-            ["7", "Drawing", "", "", "", Image.open(
-                MEDIA_ROOT+"/drawing/50.jpg")],
-            ["7", "Drawing", "", "", "", Image.open(
-                MEDIA_ROOT+"/drawing/51.jpg")],
-            ["7", "Drawing", "", "", "", Image.open(
-                MEDIA_ROOT+"/drawing/52.jpg")],
-            ["7", "Drawing", "", "", "", Image.open(
-                MEDIA_ROOT+"/drawing/53.jpg")],
-            ["7", "Drawing", "", "", "", Image.open(
-                MEDIA_ROOT+"/drawing/54.jpg")],
-            ["7", "Drawing", "", "", "", Image.open(
-                MEDIA_ROOT+"/drawing/55.jpg")],
-            ["7", "Drawing", "", "", "", Image.open(
-                MEDIA_ROOT+"/drawing/56.jpg")],
-            ["7", "Drawing", "", "", "", Image.open(
-                MEDIA_ROOT+"/drawing/57.jpg")],
-            ["7", "Drawing", "", "", "", Image.open(
-                MEDIA_ROOT+"/drawing/58.jpg")],
-            ["7", "Drawing", "", "", "", Image.open(
-                MEDIA_ROOT+"/drawing/59.jpg")],
-            ["7", "Drawing", "", "", "", Image.open(
-                MEDIA_ROOT+"/drawing/60.jpg")],
-            ["7", "Drawing", "", "", "", Image.open(
-                MEDIA_ROOT+"/drawing/61.jpg")],
-            ["7", "Drawing", "", "", "", Image.open(
-                MEDIA_ROOT+"/drawing/62.jpg")],
-            ["7", "Drawing", "", "", "", Image.open(
-                MEDIA_ROOT+"/drawing/63.jpg")],
-            ["7", "Drawing", "", "", "", Image.open(
-                MEDIA_ROOT+"/drawing/64.jpg")],
-            ["7", "Drawing", "", "", "", Image.open(
-                MEDIA_ROOT+"/drawing/65.jpg")],
-            ["7", "Drawing", "", "", "", Image.open(
-                MEDIA_ROOT+"/drawing/66.jpg")],
-            ["7", "Drawing", "", "", "", Image.open(
-                MEDIA_ROOT+"/drawing/67.jpg")],
-            ["7", "Drawing", "", "", "", Image.open(
-                MEDIA_ROOT+"/drawing/68.jpg")],
-            ["7", "Drawing", "", "", "", Image.open(
-                MEDIA_ROOT+"/drawing/69.jpg")]
+            ["7", "Drawing", "", "", "",
+                "/Users/min/Documents/liaportfolio/uploads/drawing/1.jpg"]
+            # ["7", "Drawing", "", "", "", f"drawing/2.jpg"],
+            # ["7", "Drawing", "", "", "", f"drawing/3.jpg"],
+            # ["7", "Drawing", "", "", "", f"drawing/4.jpg"],
+            # ["7", "Drawing", "", "", "", f"drawing/5.jpg"],
+            # ["7", "Drawing", "", "", "", f"drawing/6.jpg"],
+            # ["7", "Drawing", "", "", "", f"drawing/7.jpg"],
+            # ["7", "Drawing", "", "", "", f"drawing/8.jpg"],
+            # ["7", "Drawing", "", "", "", f"drawing/9.jpg"],
+            # ["7", "Drawing", "", "", "", f"drawing/10.jpg"],
+            # ["7", "Drawing", "", "", "", f"drawing/11.jpg"],
+            # ["7", "Drawing", "", "", "", f"drawing/12.jpg"],
+            # ["7", "Drawing", "", "", "", f"drawing/13.jpg"],
+            # ["7", "Drawing", "", "", "", f"drawing/14.jpg"],
+            # ["7", "Drawing", "", "", "", f"drawing/15.jpg"],
+            # ["7", "Drawing", "", "", "", f"drawing/16.jpg"],
+            # ["7", "Drawing", "", "", "", f"drawing/17.jpg"],
+            # ["7", "Drawing", "", "", "", f"drawing/18.jpg"],
+            # ["7", "Drawing", "", "", "", f"drawing/19.jpg"],
+            # ["7", "Drawing", "", "", "", f"drawing/20.jpg"],
+            # ["7", "Drawing", "", "", "", f"drawing/21.jpg"],
+            # ["7", "Drawing", "", "", "", f"drawing/22.jpg"],
+            # ["7", "Drawing", "", "", "", f"drawing/23.jpg"],
+            # ["7", "Drawing", "", "", "", f"drawing/24.jpg"],
+            # ["7", "Drawing", "", "", "", f"drawing/25.jpg"],
+            # ["7", "Drawing", "", "", "", f"drawing/26.jpg"],
+            # ["7", "Drawing", "", "", "", f"drawing/27.jpg"],
+            # ["7", "Drawing", "", "", "", f"drawing/28.jpg"],
+            # ["7", "Drawing", "", "", "", f"drawing/29.jpg"],
+            # ["7", "Drawing", "", "", "", f"drawing/30.jpg"],
+            # ["7", "Drawing", "", "", "", f"drawing/31.jpg"],
+            # ["7", "Drawing", "", "", "", f"drawing/32.jpg"],
+            # ["7", "Drawing", "", "", "", f"drawing/33.jpg"],
+            # ["7", "Drawing", "", "", "", f"drawing/34.jpg"],
+            # ["7", "Drawing", "", "", "", f"drawing/35.jpg"],
+            # ["7", "Drawing", "", "", "", f"drawing/36.jpg"],
+            # ["7", "Drawing", "", "", "", f"drawing/37.jpg"],
+            # ["7", "Drawing", "", "", "", f"drawing/38.jpg"],
+            # ["7", "Drawing", "", "", "", f"drawing/39.jpg"],
+            # ["7", "Drawing", "", "", "", f"drawing/40.jpg"],
+            # ["7", "Drawing", "", "", "", f"drawing/41.jpg"],
+            # ["7", "Drawing", "", "", "", f"drawing/42.jpg"],
+            # ["7", "Drawing", "", "", "", f"drawing/43.jpg"],
+            # ["7", "Drawing", "", "", "", f"drawing/44.jpg"],
+            # ["7", "Drawing", "", "", "", f"drawing/45.jpg"],
+            # ["7", "Drawing", "", "", "", f"drawing/46.jpg"],
+            # ["7", "Drawing", "", "", "", f"drawing/47.jpg"],
+            # ["7", "Drawing", "", "", "", f"drawing/48.jpg"],
+            # ["7", "Drawing", "", "", "", f"drawing/49.jpg"],
+            # ["7", "Drawing", "", "", "", f"drawing/50.jpg"],
+            # ["7", "Drawing", "", "", "", f"drawing/51.jpg"],
+            # ["7", "Drawing", "", "", "", f"drawing/52.jpg"],
+            # ["7", "Drawing", "", "", "", f"drawing/53.jpg"],
+            # ["7", "Drawing", "", "", "", f"drawing/54.jpg"],
+            # ["7", "Drawing", "", "", "", f"drawing/55.jpg"],
+            # ["7", "Drawing", "", "", "", f"drawing/56.jpg"],
+            # ["7", "Drawing", "", "", "", f"drawing/57.jpg"],
+            # ["7", "Drawing", "", "", "", f"drawing/58.jpg"],
+            # ["7", "Drawing", "", "", "", f"drawing/59.jpg"],
+            # ["7", "Drawing", "", "", "", f"drawing/60.jpg"],
+            # ["7", "Drawing", "", "", "", f"drawing/61.jpg"],
+            # ["7", "Drawing", "", "", "", f"drawing/62.jpg"],
+            # ["7", "Drawing", "", "", "", f"drawing/63.jpg"],
+            # ["7", "Drawing", "", "", "", f"drawing/64.jpg"],
+            # ["7", "Drawing", "", "", "", f"drawing/65.jpg"],
+            # ["7", "Drawing", "", "", "", f"drawing/66.jpg"],
+            # ["7", "Drawing", "", "", "", f"drawing/67.jpg"],
+            # ["7", "Drawing", "", "", "", f"drawing/68.jpg"],
+            # ["7", "Drawing", "", "", "", f"drawing/69.jpg"]
         ]
 
         for item in arr:
